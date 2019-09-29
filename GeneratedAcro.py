493dd5b7e4eff26817cf167ed521c1f1861a4f6d
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2019-09-29 20:17:25 +0800

    We did it xD

--- GeneratedAcro.py
+++ GeneratedAcro.py
@@ -3,10 +3,12 @@ d = {}
 f = open("C:\\Users\\MY-PC\\Desktop\\TollGateJava\\NEWFILE.txt", "w")
 wb = xlrd.open_workbook('D:\\New folder\\Book1.xlsx')
 sh = wb.sheet_by_index(3)
+x= len(sh.col_values(0))
 
-for i ,j in  :
-    print(i,j)
+for i in range(x):
+    f.write("hash.put(\"{}\",\"{}\");\n".format(  sh.cell(i,0).value,sh.cell(i,1).value))
+
 
 
 #print("hash.put("+"\""+start+"-"+destination+"\""+':'+str(value_Price[j+1])+")\n")
-#f.close()
+f.close()
