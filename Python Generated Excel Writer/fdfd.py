1b76cccbb716740996e8c228c96035bb63515e52
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2019-09-30 10:34:06 +0800

    table for Class toll gate

--- Python Generated Excel Writer/fdfd.py
+++ Python Generated Excel Writer/fdfd.py
@@ -1,6 +1,7 @@
 import xlrd
 d = {}
-f = open("D:\\New folder\\Class3Hashes.txt", "w")
+
+f = open("C:\\Users\\MY-PC\\Desktop\\TollGateJava\\tbl_Class3.txt", "w")
 wb = xlrd.open_workbook('D:\\New folder\\Book1.xlsx')
 sh = wb.sheet_by_index(2) 
 x=len(sh.col_values(0))-1
@@ -12,5 +13,5 @@ for i in range(x):
         destination = sh.cell(0,j+1).value
         value_Price = sh.row_values(i+1)
         
-        f.write("hash.put("+"\""+start+"-"+destination+"\""+':'+str(value_Price[j+1])+")\n")
+        f.write("hash.put("+"\""+start+"-"+destination+"\""+':'+str(value_Price[j+1])+");\n")
 f.close()
